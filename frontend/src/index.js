77f7e62e80d6bbd9a3a53294d046e7cb4f94bbbd
Dev-Jathu <jathusankunam005@gmail.com>
2024-08-20 12:33:39 +0530

    admin page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <AuthProvider>
+  <React.StrictMode>
     <App />
-  </AuthProvider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
